427a4eefe98af17d84ca8103d63d2911883e4167
Ubuntu <ubuntu@ip-172-31-77-250.ec2.internal>
2026-08-31 22:50:37 +0000

    update

--- test/test-package.py
+++ test/test-package.py
@@ -1,6 +1,10 @@
 import pandas as pd
 from model.predict import make_prediction
 
-sample_input_data = pd.read_csv("~/test/bankchurn_test.csv")
+from pathlib import Path
+
+sample_input_data = pd.read_csv(
+    Path(__file__).resolve().parent / "bankchurn_test.csv"
+)
 result = make_prediction(input_data=sample_input_data)
 print(result)
